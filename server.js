8a0948879aca6617ed497168bb6199c2174c52dc
Nil OKTAR <49235367+niloktar@users.noreply.github.com>
2021-12-26 13:07:52 +0300

    index html gösterildi

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const http = require ("http");
+const fs= require("fs");
 
 const requestListener = function (req,res) {
+    const index = fs.readFileSync("index.html")
     comsole.log(req)
     res.whiteHead(200);
-    res.end("Hello");
+    res.end(index.toString);
 }
 
 const server = http.createServer(requestListener);
-server.listen(8080);
+server.listen(8080); 
